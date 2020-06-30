ed8b8991f5de2a8cd2346f8b579ea4a7c2b6bf71
Harrison <mccarth@purdue.edu>
2020-06-30 19:16:34 -0400

    Added groudwork for an SQL database implementation

--- cms.py
+++ cms.py
@@ -1,5 +1,10 @@
 from frontmatter import Frontmatter
+import database
 import markdown
+
+def init():
+    dbms = database.Database(database.SQLITE, dbname='mydb.sqlite')
+    dbms.create_db_tables()
 
 def get():
     post = Frontmatter.read_file('static/journal/2020-06-30-FirstPost.md')
---
+++ database.py
@@ -0,0 +1,55 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+
+# Global Variables
+SQLITE = 'sqlite'
+
+# Table Names
+USERS           = 'users'
+ADDRESSES       = 'addresses'
+
+class Database:
+    DB_ENGINE = {
+        SQLITE: 'sqlite:///{DB}'
+    }
+
+    # Main DB Connection Ref Obj
+    db_engine = None
+    def __init__(self, dbtype, username='', password='', dbname=''):
+        dbtype = dbtype.lower()
+        if dbtype in self.DB_ENGINE.keys():
+            engine_url = self.DB_ENGINE[dbtype].format(DB=dbname)
+            self.db_engine = create_engine(engine_url)
+            print(self.db_engine)
+        else:
+            print("DBType is not found in DB_ENGINE")
+
+    def create_db_tables(self):
+        metadata = MetaData()
+        users = Table(USERS, metadata,
+                      Column('id', Integer, primary_key=True),
+                      Column('first_name', String),
+                      Column('last_name', String)
+                      )
+        address = Table(ADDRESSES, metadata,
+                        Column('id', Integer, primary_key=True),
+                        Column('user_id', None, ForeignKey('users.id')),
+                        Column('email', String, nullable=False),
+                        Column('address', String)
+                        )
+        try:
+            metadata.create_all(self.db_engine)
+            print("Tables created")
+        except Exception as e:
+            print("Error occurred during Table creation!")
+            print(e)
+
+    # Insert, Update, Delete
+    def execute_query(self, query=''):
+        if query == '' : return
+        print (query)
+        with self.db_engine.connect() as connection:
+            try:
+                connection.execute(query)
+            except Exception as e:
+                print(e)
