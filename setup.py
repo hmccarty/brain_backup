79b3d97fb12c7b903e58ea90ac31053cd70e07bc
Harrison <mccarth@purdue.edu>
2020-07-02 17:26:03 -0400

    Made application a seperate module

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='brainbank',
+    packages=['brainbank'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from application import create_app
+from brainbank import create_app
 
 app = create_app()
 
