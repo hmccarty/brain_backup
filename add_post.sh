b1d30c239a78cac5c582f5f8ceed6340ab4ff2c5
Harrison McCarty <mccarth@purdue.edu>
2020-11-21 09:32:24 -0500

    fixes and updates

--- add_post.sh
+++ add_post.sh
@@ -2,4 +2,4 @@ export FLASK_APP=wsgi.py
 export FLASK_DEBUG=1
 export DATABASE_URI=./mydb.db
 
-flask add-post ./brainbank/static/journal/2020-06-30-FirstPost.md
+flask add-post ./brainbank/static/journal/2020-07-04-TagTests.md
--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 export FLASK_APP=wsgi.py
 export FLASK_DEBUG=1
 export DATABASE_URI=./mydb.db
-
 flask run
